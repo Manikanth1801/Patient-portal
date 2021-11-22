12f9488a6b82e000bcfd2ed52dfd95bd3e6573cf
nvchaitanya <chathan2569@gmail.com>
2021-11-22 16:39:23 +0530

    Implemented JWT and Nested routes dynamically

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
+import React from "react";
 import "./App.css";
 import Header from "./Common/Header/Header";
 import Routes from "./Router/Routes";
 import { useLocation } from "react-router-dom";
 import LeftNav from "./Common/LeftNav/LeftNav";
 import Footer from "./Common/Footer/Footer";
+import { dividerClasses } from "@mui/material";
 
 function App() {
-  const location = useLocation();
-  const showHeader =
-    location.pathname.includes("patient") ||
-    location.pathname.includes("admin") ||
-    location.pathname.includes("physician");
+//   const location = useLocation();
+//   const showHeader =
+//     location.pathname.includes("patient") ||
+//     location.pathname.includes("admin") ||
+//     location.pathname.includes("physician");
 
-    localStorage.setItem('authToken', '12345')
-  return (
-    <div className="Ap">
-      {showHeader && (
-      <>
-        <Header />
-        <LeftNav />
-      </>
-      )}
-      <div style={{float:"left"}}>
-          <Routes />
-      </div>
-        {showHeader && (
-          <Footer/>
-        )}
-      {/* </div> */}
+//     localStorage.setItem('authToken', '12345')
+//   return (
+//     <div className="Ap">
+//       {showHeader && (
+//       <>
+//         <Header />
+//         <LeftNav />
+//       </>
+//       )}
+//       <div style={{float:"left"}}>
+//           <Routes />
+//       </div>
+//         {showHeader && (
+//           <Footer/>
+//         )}
+//       {/* </div> */}
+//     </div>
+//   );
+  return(
+    <div>
+      <Routes />
     </div>
-  );
+  )
 }
+
 
 export default App;
