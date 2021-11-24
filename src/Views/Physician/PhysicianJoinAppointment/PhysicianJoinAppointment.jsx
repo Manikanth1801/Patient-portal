612753572d471a6f77895fb37a56096809c10150
Siddhartha N <Siddhartha.N@citiustech.com>
2021-11-24 22:29:39 +0530

    updates from siddharth

--- src/Views/Physician/PhysicianJoinAppointment/PhysicianJoinAppointment.jsx
+++ src/Views/Physician/PhysicianJoinAppointment/PhysicianJoinAppointment.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import JoinAppointment from '../../JoinAppointment/JoinAppointment'
 
+import {Button,Input,InputGroup} from 'reactstrap'
+import MenuDivider from 'antd/lib/menu/MenuDivider'
 export default function PhysicianJoinAppointment() {
     return (
         <div>
         <JoinAppointment/>
-       <div style={{margin:"auto"}}>
-           <input type="textArea" width="500px" height="500px"/>
-       </div>
+  <InputGroup size="lg" style={{width:"600px",margin:"auto"}}>
+    <Input placeholder="Enter Prescriptions/Comments" />
+    <Button color="success" >
+      Submit
+    </Button>
+  </InputGroup>
+
 
         </div>
     )
